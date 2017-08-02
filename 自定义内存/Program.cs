919305b4be856818f6e92b444ea07cde941634f4
Rocky Zhou <xiuyangzhe@live.com>
2017-08-02 11:08:43 +0800

    Add project files.

---
+++ 自定义内存/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 自定义内存
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<string>();
+            while (true)
+            {
+                
+                int _size = Convert.ToInt32(Console.ReadLine());
+                list.Clear();
+                for (int i = 0; i < _size * 1024 * 1024; i++)
+                {
+                    list.Add("1");
+                }
+                
+            }
+        }
+    }
+}
---
+++ 自定义内存/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("自定义内存")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("自定义内存")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27721ec0-f11d-4d67-9e95-bdb9bf4677e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
